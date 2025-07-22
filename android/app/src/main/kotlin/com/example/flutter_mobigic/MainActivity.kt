e6adbcf75b9c3ba6f9a0d7474995415b66ac97a2
meghsham-mobigic <meghsham.kapure@mobigic.com>
2025-07-22 06:21:10 +0530

    ground zero

---
+++ android/app/src/main/kotlin/com/example/flutter_mobigic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_mobigic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlutterMobigicRoot());
+}
+
+class FlutterMobigicRoot extends StatelessWidget {
+  const FlutterMobigicRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
