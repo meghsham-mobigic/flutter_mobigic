49dfdde89ff1bf87dc102a8e8b9b2df57fe91449
meghsham-mobigic <meghsham.kapure@mobigic.com>
2025-08-05 10:55:10 +0530

    statusCodeData() added

--- lib/http_methods/helper/helper.dart
+++ lib/http_methods/helper/helper.dart
@@ -15,4 +15,20 @@ class Helper {
       SnackBar(content: Text(text)),
     );
   }
+
+  static String statusCodeData(int statusCode) {
+    if (statusCode >= 100 && statusCode < 200) {
+      return 'Informational';
+    } else if (statusCode >= 200 && statusCode < 300) {
+      return 'Request Successful';
+    } else if (statusCode >= 300 && statusCode < 400) {
+      return 'Request Redirection';
+    } else if (statusCode >= 400 && statusCode < 500) {
+      return 'Client Error';
+    } else if (statusCode >= 500 && statusCode < 600) {
+      return 'Server Error';
+    } else {
+      return 'Unknown Error';
+    }
+  }
 }
